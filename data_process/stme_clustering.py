eea6aac57c87016c77550b24f4892d7ce5f569e4
Coooder-X <1104143015@qq.com>
2022-12-03 18:11:22 +0800

    refactor: rename variables

--- data_process/stme_clustering.py
+++ data_process/stme_clustering.py
@@ -82,49 +82,49 @@ def stme_iteration(coord_list, total_step):
 
 def stme():
     # 纬度在前，经度在后 [latitude, longitude]
-    fileInfo = FileInfo()
-    coords = get_trip_endpoints(fileInfo, 50, False)
+    file_info = FileInfo()
+    coord_list = get_trip_endpoints(file_info, 50, False)
     coords_set = set()
-    for coord in coords:
+    for coord in coord_list:
         lon, lat = coord[0], coord[1]
         if not str(lon) + "_" + str(lat) in coords_set:
             coords_set.add(str(lon) + "_" + str(lat))
 
-    coords = np.array(coords)
-    df = pd.DataFrame({'lat': coords[:, 0], 'lon': coords[:, 1], 'type': -1, 'cluster': 0})
+    coord_list = np.array(coord_list)
+    dataframe = pd.DataFrame({'lat': coord_list[:, 0], 'lon': coord_list[:, 1], 'type': -1, 'cluster': 0})
 
     print("参数: k： " + str(k) + " kt: " + str(kt) + " min_pts: " + str(min_pts))
-    df, clusterDensityList_nor, num_clusters = STME(df, k=k, kt=kt, t_window=86400, min_pts=min_pts)  # 0.0381 751
+    dataframe, clusterDensityList_nor, num_of_clusters = STME(dataframe, k=k, kt=kt, t_window=86400, min_pts=min_pts)  # 0.0381 751
     # earth's radius in km
-    cluster_labels = df['cluster']
-    raito = len(cluster_labels[cluster_labels[:] == -1]) / len(cluster_labels)  # 计算噪声点个数占总数的比例
+    label_of_clusters = dataframe['cluster']
+    ratio = len(label_of_clusters[label_of_clusters[:] == -1]) / len(label_of_clusters)  # 计算噪声点个数占总数的比例
     # num_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)  # 获取分簇的数目
-    print('rattio:' + str(raito))
-    print('Clustered ' + str(len(coords)) + ' points to ' + str(num_clusters) + ' clusters')
-    df.to_csv("../spatio_splits/spatio_point_split_" + str(k) + "_" + str(kt) + "_" + str(min_pts) + ".csv")
+    print('ratio:' + str(ratio))
+    print('Clustered ' + str(len(coord_list)) + ' points to ' + str(num_of_clusters) + ' clusters')
+    dataframe.to_csv("../spatio_splits/spatio_point_split_" + str(k) + "_" + str(kt) + "_" + str(min_pts) + ".csv")
 
     # print('coords', coords)
     # 所有簇的点组成的面
     hulls = []
-    fig = plt.figure(figsize=(20, 10))
-    ax = fig.subplots()
-    for p in coords[cluster_labels == -1]:
+    figure = plt.figure(figsize=(20, 10))
+    axis = figure.subplots()
+    for p in coord_list[label_of_clusters == -1]:
         x, y = p[0], p[1]
-        ax.scatter(x, y, c='#000', marker='o', s=10)
-    for n in range(num_clusters):
+        axis.scatter(x, y, c='#000', marker='o', s=10)
+    for n in range(num_of_clusters):
         # print(n, coords[cluster_labels == n + 1])
         points = []
-        color = randomcolor()
-        for p in coords[cluster_labels == n + 1]:
+        cur_color = randomcolor()
+        for p in coord_list[label_of_clusters == n + 1]:
             x, y = p[0], p[1]
             points.append(Point(x, y))
-            ax.scatter(x, y, c=color, marker='o', s=10)
+            axis.scatter(x, y, c=cur_color, marker='o', s=10)
         # points = [Point(i, j) for i, j in coords[cluster_labels == n + 1]]
-        multipoints = MultiPoint(points)
-        hulls.append(multipoints.convex_hull)
+        multi_points = MultiPoint(points)
+        hulls.append(multi_points.convex_hull)
 
-    ax.set_xlabel('lon')  # 画出坐标轴
-    ax.set_ylabel('lat')
+    axis.set_xlabel('lon')  # 画出坐标轴
+    axis.set_ylabel('lat')
     plt.savefig(f'../../figs/k{k}_kt{kt}_mpts{min_pts}.png')
     plt.show()
     return hulls
