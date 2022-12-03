2058ac543875e6462e65e33254bc62ebe5e57dc7
Coooder-X <1104143015@qq.com>
2022-12-03 17:57:39 +0800

    feat(轨迹端点聚类): 对噪声点的多次迭代聚类

--- data_process/stme_clustering.py
+++ data_process/stme_clustering.py
@@ -17,35 +17,71 @@ kt = 5  # 10
 min_pts = 10
 
 
-def stme_ones(coords, start_cluster_idx):
-    df = pd.DataFrame({'lat': coords[:, 0], 'lon': coords[:, 1], 'type': -1, 'cluster': start_cluster_idx})
-    df, clusterDensityList_nor, num_clusters = STME(df, k=k, kt=kt, t_window=86400, min_pts=min_pts)  # 0.0381 751
-    return df, clusterDensityList_nor, num_clusters
+def get_noise_index_list(df_col, coord_list):
+    """
+    :param df_col: 当前 dataFrame 中 ‘cluster’ 列的标签数据(已转换成列表)，元素是 number 类型
+    :param coord_list: 当前 坐标列表
+    :return 返回 所有噪声点在 coords 中的索引，所有噪声点坐标
+    """
+    noise_index_lst = []
+    noise_coord_lst = []
+    lst = df_col
+    for i, label in enumerate(lst):
+        if label == -1:
+            noise_index_lst.append(i)
+            noise_coord_lst.append(coord_list[i])
+    return noise_index_lst, noise_coord_lst
 
 
-def stme_iteration(coords, total_step):
-    start_cluster_idx = 0
-    total_cluster_labels = []
+def stme_ones(coord_list, start_cluster_idx):
+    dataframe = pd.DataFrame({'lat': coord_list[:, 0], 'lon': coord_list[:, 1], 'type': -1, 'cluster': 0})
+    dataframe, clusterDensityList_nor, num_of_clusters = STME(dataframe, k=k, kt=kt, t_window=86400, min_pts=min_pts)
+    print('stme_ones\n', dataframe['cluster'].values.tolist())
+    labels = dataframe['cluster'].values.tolist()
+    labels = [x + start_cluster_idx if x != -1 else x for x in labels]
+    dataframe['cluster'] = np.array(labels)
+    print(dataframe['cluster'].values.tolist())
+    return dataframe, clusterDensityList_nor, num_of_clusters
+
+
+def stme_iteration(coord_list, total_step):
+    """
+    :param coord_list 总的要聚类的点列表
+    :param total_step 第一次聚类后，需要继续聚类的次数，即总聚类次数为 total_step + 1
+    :return total_df 总的聚类得到的 dataFrame, total_num_clusters 聚类的簇的个数 (最大簇 id + 1)
+    """
+    start_cluster_idx = 0  # 每轮聚类开始的簇的编号
     total_num_clusters = 0
-    total_df, clusterDensityList_nor, num_clusters = stme_ones(coords, start_cluster_idx)
-    cur_cluster_labels = total_df['cluster']
-    total_cluster_labels.extend(cur_cluster_labels)
-    cur_noise_coords = coords[cur_cluster_labels == -1]
-    for i in range(total_step):
-        print('当前剩余噪声点个数: ', cur_noise_coords)
-        start_cluster_idx = max(cur_cluster_labels)
-        cur_df, clusterDensityList_nor, num_clusters = stme_ones(cur_noise_coords, start_cluster_idx)
-        cur_cluster_labels = cur_df['cluster']
-        cur_noise_coords = coords[cur_cluster_labels == -1]
+    total_df, clusterDensityList_nor, num_of_clusters = stme_ones(coord_list, start_cluster_idx)
+    total_cluster_labels = total_df['cluster'].values.tolist()  # 总的聚类簇 id 列表
+    # 第一轮聚类得到的噪声数组、噪声点在原数组的索引列表
+    noise_index, cur_noise_coords = get_noise_index_list(total_cluster_labels, coord_list)
 
-        total_df.concat([total_df, cur_df], axis=0)
-        total_cluster_labels.extend(cur_cluster_labels)
-        total_num_clusters = max(total_num_clusters, num_clusters)
-    return total_df, total_num_clusters, total_num_clusters
+    for i in range(total_step):
+        # 聚类时要计算 k 邻近，因此输入的点个数不能小于 k
+        if len(cur_noise_coords) < k:
+            break
+        print('当前剩余噪声点个数: ', len(cur_noise_coords))
+        start_cluster_idx = max(total_df['cluster']) + 1
+        # 本轮聚类的对象是上次聚类中的噪声点，聚类的初始簇 id 是 上次聚类最大 id + 1 （start_cluster_idx）
+        cur_df, clusterDensityList_nor, num_of_clusters = stme_ones(np.array(cur_noise_coords), start_cluster_idx)
+        # 基于上次的噪声点数组聚类结果，计算出新的、索引与 cur_noise_coords 对应的簇 label 数组
+        cur_cluster_labels = cur_df['cluster']
+        # noise_index 是 cur_noise_coords 噪声点对应原点集的索引，本轮噪声聚类后，把本轮聚类结果更新到总聚类结果中
+        for idx, label in enumerate(cur_cluster_labels):
+            total_cluster_labels[noise_index[idx]] = label
+        print('总聚类标签', total_cluster_labels)
+        total_df['cluster'] = np.array(total_cluster_labels)
+        # 本轮聚类后，检查剩下的噪声点，归到一个数组，并记录它们的索引，由下一轮使用
+        noise_index, cur_noise_coords = get_noise_index_list(total_cluster_labels, coord_list)
+        # 更新总的簇个数
+        total_num_clusters = start_cluster_idx + num_of_clusters
+
+    return total_df, total_num_clusters
 
 
 def stme():
-    ## 纬度在前，经度在后 [latitude, longitude]
+    # 纬度在前，经度在后 [latitude, longitude]
     fileInfo = FileInfo()
     coords = get_trip_endpoints(fileInfo, 50, False)
     coords_set = set()
@@ -100,14 +136,16 @@ if __name__ == "__main__":
     fileInfo = FileInfo()
     coords = get_trip_endpoints(fileInfo, 50, False)
     coords = np.array(coords)
-    df, cluster_labels, num_clusters = stme_iteration(coords, 3)
+    df, num_clusters = stme_iteration(coords, 4)
+    print(f'簇个数：{num_clusters}')
+    cluster_labels = df['cluster']
     fig = plt.figure(figsize=(20, 10))
     ax = fig.subplots()
+    print(coords.shape, cluster_labels.shape)
     for p in coords[cluster_labels == -1]:
         x, y = p[0], p[1]
         ax.scatter(x, y, c='#000', marker='o', s=10)
     for n in range(num_clusters):
-        # print(n, coords[cluster_labels == n + 1])
         points = []
         color = randomcolor()
         for p in coords[cluster_labels == n + 1]:
@@ -120,6 +158,6 @@ if __name__ == "__main__":
 
     ax.set_xlabel('lon')  # 画出坐标轴
     ax.set_ylabel('lat')
-    # plt.savefig(f'../../figs/k{k}_kt{kt}_mpts{min_pts}.png')
+    plt.savefig(f'../../figs/迭代聚类_k{k}_kt{kt}_mpts{min_pts}.png', dpi=300)
     plt.show()
     # return hulls
